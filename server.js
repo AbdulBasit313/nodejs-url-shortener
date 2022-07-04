4d538b7f477a442f8e9fb0e3cde70a783448eb5d
Abdul Basit <abdul_basit313@outlook.com>
2022-07-04 23:29:12 +0500

    fix: port type fix

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use(express.json())
 // ROUTE
 app.use('/', router)
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`))
